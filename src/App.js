1db52dd8cbaa17dc7b9329a29e6b25f1b10711ff
DGilian <g.denis@neolynk.fr>
2018-07-23 18:47:19 +0200

    add background images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import backgroundGrey from './images/Fond.png'
+import vegetation from './images/Image2.png'
+import viewtemp from './images/cars_view.jpg'
 
 class App extends Component {
   render() {
     return (
       <main className="App">
-        <p>hello world</p>
+        <section>
+          <img src={backgroundGrey} />
+          <img src={vegetation} />
+         <p>hello world</p>
+        </section>
+       <section>
+
+       </section>
+       <img src={viewtemp}/>
       </main>
     );
   }
